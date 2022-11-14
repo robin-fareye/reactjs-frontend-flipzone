d333420ea165e8a3b9e9de0bf3778dc172703d03
Parth Somani <parth.somani@getfareye.com>
2022-11-14 12:22:01 +0530

    retailer basic done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Login from './component/auth/Login';
-//import Home from './component/home/Home';
+import Home from './component/home/Home';
 import Cart from './component/Cart/Cart';
 import ProductCatalouge from './component/ProductCatalogue/ProductCatalouge';
 import ProductDetails from './component/ProductDetails/ProductDetails';
+import RetailerAddProduct from './component/retailer/RetailerAddProduct';
+
 function App() {
   return (
     <div>
-      <Cart/>
+      <RetailerAddProduct/>
     </div>
   );
 }
