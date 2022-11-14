71dc32f16fa18b5ec1c468ace091a50eb1ebc5cd
Sohil lamba <sohil.lamba@getfareye.com>
2022-11-14 11:12:12 +0530

    Header and Category

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// import React from 'react';
 import './App.css';
 import Login from './component/auth/Login';
+import Category from './component/category/Category';
+import Header from './component/header/Header';
 //import Home from './component/home/Home';
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Category/>
       <Login/>
     </div>
   );
